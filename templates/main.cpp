a3da56ff0dc1f4ecc63c622ad36e663f20a5c55f
Nicolas Alba <nicolas.alba@fundacion-jala.org>
2022-09-19 01:20:46 -0400

    Nico's macros added

--- templates/main.cpp
+++ templates/main.cpp
@@ -11,6 +11,9 @@ using namespace std;
 
 #define each(x, xs)  for (auto &x : (xs))
 #define rep(i, be, en) for (__typeof(en) i = (be) - ((be) > (en)); i != (en) - ((be) > (en)); i += 1 - 2 * ((be) > (en)))
+#define FOR(i, a, b) for (ll (i) = (a); (i) < (b); (i)++)
+#define ROF(i, a, b) for (ll (i) = (a); (i) >=  (b); (i)--)
+#define REP(i, a, b) for (ll (i) = (a); (i) <= (b); (i)++)
 
 using ll = long long;
 using ld = long double;
@@ -21,12 +24,18 @@ using vi = vector<int>;
 using vb = vector<bool>;
 using vl = vector<ll>;
 using vs = vector<string>;
+typedef vector<vl> vvl;
+typedef vector<pl> vpl;
+template<class T> using pql = priority_queue<T,vector<T>,greater<T>>;
+template<class T> using pqg = priority_queue<T>;
 
 // >>>>>>>>>> debugging >>>>>>>>>>
 #ifdef LOCAL
     #include DEBUG
+    #define LINE  cout << "-------------------" << endl;
 #else
     #define dbg(x...)
+    #define LINE
 #endif
 // <<<<<<<<<< debugging <<<<<<<<<<
 
@@ -45,7 +54,22 @@ void _(string s) {
 }
 
 const ll INF = INT64_MAX;
+const int inf = INT32_MAX;
+const ld PI = acos(-1);
 const int MOD = 1e9 + 7;
+const int DX[4]{1,0,-1,0}, DY[4]{0,1,0,-1};
+// <<<<<<<<<<<<<< Random Numbers <<<<<<<<<<<<<
+mt19937 mt_rng(chrono::steady_clock::now().time_since_epoch().count());
+ll randint(ll a, ll b) {
+    return uniform_int_distribution<ll>(a, b)(mt_rng);
+}
+
+// <<<<<<<<<<<<<<< Ordered Set <<<<<<<<<<<<<<<<<<<
+/*#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define oset tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+find_by_order(k) order_of_key(k) */
 
 void test_case();
 
