e3ddde10c08644ad4c8f11917e0d8c4b73be22ed
Nicolas Alba <nicolas.alba@fundacion-jala.org>
2022-09-20 23:03:33 -0400

    Random generator and oset deleted from template

--- templates/main.cpp
+++ templates/main.cpp
@@ -12,8 +12,6 @@ using namespace std;
 #define each(x, xs)  for (auto &x : (xs))
 #define rep(i, be, en) for (__typeof(en) i = (be) - ((be) > (en)); i != (en) - ((be) > (en)); i += 1 - 2 * ((be) > (en)))
 #define FOR(i, a, b) for (ll (i) = (a); (i) < (b); (i)++)
-#define ROF(i, a, b) for (ll (i) = (a); (i) >=  (b); (i)--)
-#define REP(i, a, b) for (ll (i) = (a); (i) <= (b); (i)++)
 
 using ll = long long;
 using ld = long double;
@@ -24,8 +22,8 @@ using vi = vector<int>;
 using vb = vector<bool>;
 using vl = vector<ll>;
 using vs = vector<string>;
-typedef vector<vl> vvl;
-typedef vector<pl> vpl;
+using vvl = vector<vl>;
+using vpl = vector<pl>;
 template<class T> using pql = priority_queue<T,vector<T>,greater<T>>;
 template<class T> using pqg = priority_queue<T>;
 
@@ -58,18 +56,6 @@ const int inf = INT32_MAX;
 const ld PI = acos(-1);
 const int MOD = 1e9 + 7;
 const int DX[4]{1,0,-1,0}, DY[4]{0,1,0,-1};
-// <<<<<<<<<<<<<< Random Numbers <<<<<<<<<<<<<
-mt19937 mt_rng(chrono::steady_clock::now().time_since_epoch().count());
-ll randint(ll a, ll b) {
-    return uniform_int_distribution<ll>(a, b)(mt_rng);
-}
-
-// <<<<<<<<<<<<<<< Ordered Set <<<<<<<<<<<<<<<<<<<
-/*#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define oset tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
-find_by_order(k) order_of_key(k) */
 
 void test_case();
 
